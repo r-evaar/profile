525f10d649d1d906ef020f473e350b3b6ed0faf7
Abdalla Rashed <r.abdalla.1195@gmail.com>
2023-05-19 22:21:12 +0400

    first commit

---
+++ static/js/loading.js
@@ -0,0 +1,5 @@
+$(function() {$.get('static/html/navbar.html', function(data) {$('#navbar-main').html(data);});});
+
+$(function() {$.get('static/html/cover.html', function(data) {$('#cover-main').html(data);});});
+
+$(function() {$.get('static/html/skills.html', function(data) {$('#skills-main').html(data);});});
