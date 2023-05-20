07bc4e18bea3b33074eda4fbdc7e9efe2c190880
Abdalla Rashed <r.abdalla.1195@gmail.com>
2023-05-20 11:42:58 +0400

    footer added

--- static/js/loading.js
+++ static/js/loading.js
@@ -5,3 +5,5 @@ $(function() {$.get('static/html/cover.html', function(data) {$('#cover-main').h
 $(function() {$.get('static/html/skills.html', function(data) {$('#skills-main').html(data);});});
 
 $(function() {$.get('static/html/projects.html', function(data) {$('#projects-main').html(data);});});
+
+$(function() {$.get('static/html/contact.html', function(data) {$('#contact-main').html(data);});});
