068a075bc410b3a0c29a7aa64d201e83aacba66c
Abdalla Rashed <r.abdalla.1195@gmail.com>
2023-06-18 06:29:14 +0400

    fixed images preload bug again

--- static/js/projects.js
+++ static/js/projects.js
@@ -2,17 +2,19 @@
 var carousel = document.getElementById('carouselExampleIndicators');
 // Get all the images in the carousel
 var images = carousel.querySelectorAll('.proj-img');
-// Loop through the images and load them asynchronously
 
+// Loop through the images and load them asynchronously
 urls = [
     'static/assets/imgs/fod.jpg',
     'static/assets/imgs/ais.jpg',
     'static/assets/imgs/ega2.jpg',
     'static/assets/imgs/ega3.jpg'
-]
-
+];
 for (var i = 0; i < images.length; i++) {
-    images[i].style.backgroundImage = 'url(' + urls[i] + ')';  
+    images[i].src = urls[i];  
+    images[i].onload = function() {
+        this.style.objectFit = 'cover';
+    };
 }
 
 $('#carouselExampleIndicators').carousel({
